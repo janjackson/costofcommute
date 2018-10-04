eb89fb11f5ca6f1977e6d58075007fa5521a0551
Adam Janes <adam@Adams-MacBook-Air-2.local>
2018-10-04 22:27:16 +0200

    Improve map

--- src/D3Map.js
+++ src/D3Map.js
@@ -9,23 +9,36 @@ import 'leaflet-routing-machine';
     locations - Array - [{ lat: ###, lng: ### }, { lat: ###, lng: ### }, { lat: ###, lng: ### }, ...] 
 */
 const D3Map = (routes, home, locations) => {
-    var map = L.map('map').setView([52.19226, 0.15216], 13);
+    const map = L.map('map').setView([52.19226, 0.15216], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}{r}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
     Promise.all(routes).then((routes) => {
+        const lengthScale = d3.scaleLinear()
+            .domain(d3.extent(locations, d => d.number))
+            .range([2, 7])
+
+        // Add lines + markers for each location
         routes.forEach((route, i) => {
             const coordinates = route.shape.map(point => point.split(","))
             L.polyline(coordinates, {
-                weight: (i * 3) + 2
+                weight: lengthScale(locations[i].number)
             }).addTo(map);
             L.marker([route.waypoint[1].mappedPosition.latitude, route.waypoint[1].mappedPosition.longitude]).addTo(map);
         })
+
+        // Map fits all routes that we want to show
+        const corner1 = [d3.max(routes, (d) => d.boundingBox.topLeft.latitude), d3.min(routes, (d) => d.boundingBox.topLeft.longitude)]
+        const corner2 = [d3.min(routes, (d) => d.boundingBox.bottomRight.latitude), d3.max(routes, (d) => d.boundingBox.bottomRight.longitude)]
+        map.fitBounds([
+            corner1,
+            corner2
+        ])
     })
 
-    var homeIcon = L.icon({
+    const homeIcon = L.icon({
         iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Home_font_awesome.svg/512px-Home_font_awesome.svg.png',
         iconSize: [50, 50]
     }); 
@@ -33,7 +46,6 @@ const D3Map = (routes, home, locations) => {
     L.marker([home.lat, home.lng], {
         icon: homeIcon
     }).addTo(map);
-
 
 }
 
