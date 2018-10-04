d13dc17c4f254ab6a343acc8d99794ada953f65d
Adam Janes <adam@Adams-MacBook-Air-2.local>
2018-10-04 15:47:33 +0200

    More setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Inputs from './Inputs'
+import Map from './Map'
+import Results from './Results'
 
 class App extends Component {
 	render() {
+		const hello = "Helldlksjo"
 		return (
-			<div>Hello World</div>
-    	);
+			<div>
+				<Inputs />
+				<Map />
+				<Results />
+			</div>
+		);
 	}
 }
 
---
+++ src/Results.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Results = () => {
+    return (
+        <div>
+            Results go here
+        </div>
+    )
+}
+
+export default Results
