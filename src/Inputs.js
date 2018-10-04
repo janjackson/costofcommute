bb1a0af613c7d70f637e10226f12779387325197
fabian_z <donaldducktrash@gmail.com>
2018-10-04 22:07:22 +0200

    AutoSuggestion for first adress field

--- src/Inputs.js
+++ src/Inputs.js
@@ -10,9 +10,9 @@ export default class Inputs extends Component {
         };
     }
     handleClick(){
-        var homeAddress = this.refs.home.value
-        var locations = this.state.locations
-        var opportunityCosts = this.refs.opportunityCosts.value
+        const homeAddress = this.refs.home.value
+        const locations = this.state.locations
+        const opportunityCosts = this.refs.opportunityCosts.value
         console.log('homeAddress, loc1name, loc1number ', homeAddress, locations, opportunityCosts)
     }
     addLoc(){
@@ -22,15 +22,15 @@ export default class Inputs extends Component {
         });
     }
     handleChangeLocation = (event) => {
-      var index = event.target.getAttribute('name')
-      var state = this.state
+      const index = event.target.getAttribute('name')
+      const state = this.state
       console.log('state ', state, index);
       state.locations[index].location = event.target.value
       this.setState(state)
     }
     handleChangeNumber = (event) => {
-      var index = event.target.getAttribute('name')
-      var state = this.state
+      const index = event.target.getAttribute('name')
+      const state = this.state
       state.locations[index].number = event.target.value
       this.setState(state)
     }
@@ -39,24 +39,25 @@ export default class Inputs extends Component {
             <div>
               <label htmlFor="pot-house">Adress of potential real estate</label>
               <div className="input-group">
-                <span className="input-group-addon"><i className="fas fa-map-marker-alt"></i></span>
-                <input id="pot-house" type="text" className="form-control" ref="home" placeholder="Adresse"/><br/>
+                <span className="input-group-addon"><i className="fas fa-home"></i></span>
+                <Geosuggest placeholder="Adress"/>
+                {/*<!--id="pot-house" type="text" className="form-control" ref="home" placeholder="Adress"-->*/}
               </div>
 
               <label htmlFor="time-in-money">How much do you value your time in money?</label>
               <div className="input-group">
                 <span className="input-group-addon"><i className="fas fa-euro-sign"></i></span>
-                <input id="time-in-money" type="text" className="form-control" ref="opportunityCosts" placeholder="Betrag in Euro"/><br/>
+                <input id="time-in-money" type="text" className="form-control" ref="opportunityCosts" placeholder="Amount of money in Euro"/><br/>
               </div>
 
-              <Geosuggest />
+              <label>Most visited places in one week (e.g. work place or college)</label>
               { this.state.locations.map((val, index)=>{
-                  return(
-                      <div key={index}>
-                      <input type="text" name={index} value={this.state.locations[index].location} onChange={this.handleChangeLocation}/><br/>
-                      <input type="number" name={index} value={this.state.locations[index].number} onChange={this.handleChangeNumber}/><br/>
-                      </div>
-                  )
+                return(
+                  <div key={index}>
+                    <input type="text" name={index} value={this.state.locations[index].location} onChange={this.handleChangeLocation}/><br/>
+                    <input type="number" name={index} value={this.state.locations[index].number} onChange={this.handleChangeNumber}/><br/>
+                  </div>
+                )
               })}
               <button onClick={this.addLoc.bind(this)}>+</button><br/>
               <button onClick={this.handleClick.bind(this)}>Add</button>
