bb0b7138c1da749b328bfda1c78ff3e49b91843a
fabian_z <donaldducktrash@gmail.com>
2018-10-05 06:06:55 +0200

    Fcking bug

--- src/Input/Places.js
+++ src/Input/Places.js
@@ -1,54 +1,80 @@
 import React, { Component } from 'react'
 import Geosuggest from "react-geosuggest";
 
-export default class Inputs extends Component {
+export default class Places extends Component {
   constructor() {
     super();
     this.state = {
-      locations: [{ location: '', number: 0 }]
-    };
+      currentTimePerWeek: 1,
+      currentAdress: "",
+      currentCoordinate: "",
+      listItems: []
+  };
   }
 
-  handleClick() {
-    var homeAddress = this.refs.home.value
-    var locations = this.state.locations
-    var opportunityCosts = this.refs.opportunityCosts.value
-    console.log('homeAddress, loc1name, loc1number ', homeAddress, locations, opportunityCosts)
-  }
-  addLoc() {
+  updateTimesPerWeek = (e) => {
     this.setState({
-      currentNumber: this.state.currentNumber + 1,
-      locations: this.state.locations.concat([{ location: '', number: 0 }])
-    });
+      currentTimePerWeek: e.target.value,
+    })
   }
-  handleChangeLocation = (event) => {
-    var index = event.target.getAttribute('name')
-    var state = this.state
-    console.log('state ', state, index);
-    state.locations[index].location = event.target.value
-    this.setState(state)
+
+  onSuggestSelect(suggest) {
+    try {
+      this.setState({
+        currentCoordinate: suggest.location,
+        currentAdress: suggest.gmaps.formatted_address
+      })
+    }
+
+    catch(e){
+      console.log("Invalid Address");
+    }
   }
-  handleChangeNumber = (event) => {
-    var index = event.target.getAttribute('name')
-    var state = this.state
-    state.locations[index].number = event.target.value
-    this.setState(state)
+
+  addPlace(){
+    var singleItem = [this.state.currentAdress, this.state.currentTimePerWeek, this.state.currentCoordinate];
+    var newItems = this.state.listItems.push(singleItem)
+    this.setState({
+      listItems: newItems
+    })
+    console.log(this.state.listItems);
+
+
+
+
+
+
+  }
+
+  renderListElements() {
+    console.log(this.state.listItems)
+    return this.state.listItems.map((item, index) =>
+      <li key={index}>{item}</li>
+    )
   }
 
   render() {
     return (
-      <div>
-        { this.state.locations.map((val, index)=>{
-          return(
-            <div key={index}>
-              <input type="text" name={index} value={this.state.locations[index].location} onChange={this.handleChangeLocation}/><br/>
-              <input type="number" name={index} value={this.state.locations[index].number} onChange={this.handleChangeNumber}/><br/>
-            </div>
-          )
-        })}
-        <button onClick={this.addLoc.bind(this)}>+</button><br/>
-        <button onClick={this.handleClick.bind(this)}>Add</button>
+      <div className="add-places-container">
+        <input onChange={this.updateTimesPerWeek.bind(this)} defaultValue="1" min="1" className="form-control-sg" type="number"/>
+        <span> time(s)/week to </span>
+
+        <div className="input-group sg">
+          <span className="input-group-addon"><i className="fas fa-map-marker-alt"></i></span>
+          <Geosuggest placeholder="Address"
+                      onSuggestSelect={this.onSuggestSelect.bind(this)}/>
+          <button className="button-primary" onClick={this.addPlace.bind(this)}>Add</button>
+        </div>
+
+        console.log(this.state.listItems)
+
+        <ol>{this.renderListElements()}</ol>
+
+
+
       </div>
     )
   }
+
+
 }
